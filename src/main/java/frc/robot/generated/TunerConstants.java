13e340da91b57c0843621a40302c9927a0f09d62
Praneel Patel <tlm.dev.BR@gmail.com>
2026-01-07 15:35:34 -0600

    Changed wheel radius parameter to 1 inch to resolve odometry issues

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -82,7 +82,7 @@ public class TunerConstants {
 
     private static final double kDriveGearRatio = 6.75;
     private static final double kSteerGearRatio = 13.371428571428572;
-    private static final Distance kWheelRadius = Inches.of(2);
+    private static final Distance kWheelRadius = Inches.of(1);
 
     private static final boolean kInvertLeftSide = false;
     private static final boolean kInvertRightSide = true;
